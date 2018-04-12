598566532f80a93492bcbf50debc48a9a6751d6f
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-12 17:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 
 //Scooter with year, color, and model properties
 //Driver with name, age, and experience properties
-//PickupLocation with address and city properties 
-//should create a new scooter with three properties 
+//PickupLocation with address and city properties
+//should create a new scooter with three properties
 
 function Scooter (year, color, model) {
    this.year = year;
    this.color = color;
    this.model = model;
+}
+
+function Driver (name, age, experience) {
+   this.name = name;
+   this.age = age;
+  this.experience = experience;
 }
 
